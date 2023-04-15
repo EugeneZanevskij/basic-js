2bcd275cc59e3106c96bfddb6152aa78b077ff4e
Eugene Zanevskij <zanevskijzenia@gmail.com>
2023-04-15 19:15:42 +0300

    feat: extended-repeater.js '

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,10 +15,38 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  // The str and addition parameters are strings by default. In case when type of these parameters is different, they must be converted to a string.
+  str = String(str);
+  let addition = options.addition !== undefined ? String(options.addition) : '';
+
+  let separator = options.separator !== undefined ? options.separator : '+';
+  let additionSeparator = options.additionSeparator !== undefined ? options.additionSeparator : '|';
+  let repeatTimes = options.repeatTimes !== undefined ? options.repeatTimes : 1;
+  let additionRepeatTimes = options.additionRepeatTimes !== undefined ? options.additionRepeatTimes : 1;
+
+  let stringsArray = [];
+
+  // Repeat the str with addition
+  for (let i = 0; i < repeatTimes; i++) {
+    let strRepeat = str;
+    if (addition !== '') {
+      for (let j = 0; j < additionRepeatTimes; j++) {
+        strRepeat += addition;
+        if (j < additionRepeatTimes - 1) {
+          strRepeat += additionSeparator;
+        }
+      }
+    }
+    stringsArray.push(strRepeat);
+  }
+
+  // Join the repeated strings with separator
+  let result = stringsArray.join(separator);
+
+  return result;
 }
+
 
 module.exports = {
   repeater
