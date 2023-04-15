e01bc63fd2357f87c160a97c588f690c4f33bb36
Eugene Zanevskij <zanevskijzenia@gmail.com>
2023-04-15 22:35:40 +0300

    feat: vigenere-cipher.js

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,64 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(isDirect = true) {
+    this.isDirect = isDirect;
+    this.alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  
+  encrypt(message, key) {
+    if (message === undefined || key === undefined) {
+      throw new Error("Incorrect arguments!");
+    }
+
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+    let encryptedMessage = '';
+    let keyIndex = 0;
+
+    for (let i = 0; i < message.length; i++) {
+      const char = message[i];
+      if (this.alphabet.includes(char)) {
+        const keyChar = key[keyIndex % key.length];
+        const keyChIndex = this.alphabet.indexOf(keyChar);
+        const chIndex = this.alphabet.indexOf(char);
+        const encryptedChIndex = (chIndex + keyChIndex) % this.alphabet.length;
+        const encryptedCh = this.alphabet[encryptedChIndex];
+        encryptedMessage += encryptedCh;
+        keyIndex++;
+      } else {
+        encryptedMessage += char;
+      }
+    }
+
+    return this.isDirect ? encryptedMessage : encryptedMessage.split('').reverse().join('');
+  }
+  decrypt(encryptedMessage, key) {
+    if (encryptedMessage === undefined || key === undefined) {
+      throw new Error("Incorrect arguments!");
+    }
+
+    encryptedMessage = encryptedMessage.toUpperCase();
+    key = key.toUpperCase();
+    let message = '';
+    let keyIndex = 0;
+
+    for (let i = 0; i < encryptedMessage.length; i++) {
+      const char = encryptedMessage[i];
+      if (this.alphabet.includes(char)) {
+        const keyChar = key[keyIndex % key.length];
+        const keyChIndex = this.alphabet.indexOf(keyChar);
+        const chIndex = this.alphabet.indexOf(char);
+        const decryptedChIndex = (chIndex - keyChIndex + this.alphabet.length) % this.alphabet.length;
+        const decryptedCh = this.alphabet[decryptedChIndex];
+        message += decryptedCh;
+        keyIndex++;
+      } else {
+        message += char;
+      }
+    }
+
+    return this.isDirect ? message : message.split('').reverse().join('');
   }
 }
 
